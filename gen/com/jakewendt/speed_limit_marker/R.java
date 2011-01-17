cf46f266144900cdaeab156b7086e2f0f6985ff6
Jake <github@jakewendt.com>
2011-01-17 00:45:19 -0800

    Added some speech and removed the classes from repo.

--- gen/com/jakewendt/speed_limit_marker/R.java
+++ gen/com/jakewendt/speed_limit_marker/R.java
@@ -8,32 +8,42 @@
 package com.jakewendt.speed_limit_marker;
 
 public final class R {
+    public static final class array {
+        public static final int units=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int speed_limit_sign=0x7f020001;
     }
     public static final class id {
-        public static final int bearing=0x7f050003;
-        public static final int cog=0x7f050004;
-        public static final int decrease=0x7f050000;
-        public static final int increase=0x7f050001;
-        public static final int latitude=0x7f050005;
-        public static final int longitude=0x7f050006;
-        public static final int mark=0x7f050008;
-        public static final int showlocation=0x7f050007;
-        public static final int speedlimit=0x7f050002;
+        public static final int cog=0x7f060003;
+        public static final int decrease=0x7f060000;
+        public static final int distance=0x7f060004;
+        public static final int email_field=0x7f060009;
+        public static final int increase=0x7f060001;
+        public static final int latitude=0x7f060005;
+        public static final int longitude=0x7f060006;
+        public static final int mark=0x7f060007;
+        public static final int mph_kph_language=0x7f06000a;
+        public static final int speedlimit=0x7f060002;
+        public static final int username_field=0x7f060008;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int settings=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int back=0x7f040002;
-        public static final int clear=0x7f040003;
-        public static final int decrease=0x7f040004;
-        public static final int hello=0x7f040000;
-        public static final int increase=0x7f040005;
-        public static final int mark=0x7f040006;
+        public static final int app_name=0x7f050001;
+        public static final int back=0x7f050002;
+        public static final int decrease=0x7f050004;
+        public static final int email_text=0x7f050008;
+        public static final int hello=0x7f050000;
+        public static final int increase=0x7f050005;
+        public static final int mark=0x7f050006;
+        public static final int settings=0x7f050003;
+        public static final int units_text=0x7f050009;
+        public static final int username_text=0x7f050007;
     }
 }
--- src/com/jakewendt/speed_limit_marker/SpeedLimitMarker.java
+++ src/com/jakewendt/speed_limit_marker/SpeedLimitMarker.java
@@ -2,20 +2,24 @@
 package com.jakewendt.speed_limit_marker;
 
 import com.jakewendt.speed_limit_marker.R;
+import com.jakewendt.speed_limit_marker.Settings;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.widget.Button;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -36,56 +40,91 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
+import android.util.TypedValue;
+public class SpeedLimitMarker extends Activity implements TextToSpeech.OnInitListener {
 
-public class SpeedLimitMarker extends Activity {
-
+	private static final String TAG = "SpeedLimit";
+	private TextToSpeech tts;
+	static final int TTS_CHECK_CODE = 0;
+ 
+	/* I need to learn the differences between public, private, final, static, etc. */
+	private Location bearing_location;
+	private float last_bearing = Float.NaN;
+	private float distance;
 	private LocationManager locationManager;
-
 	public static final String PREFS_NAME = "MyPrefsFile";
-
-    public int speed;
-    public String name;
-    public String email;
-
-    public boolean mSilentMode = false;
-
-    static final private int BACK_ID = Menu.FIRST;
-    static final private int CLEAR_ID = Menu.FIRST + 1;
+	public boolean mSilentMode = false;
+	static final private int BACK_ID = Menu.FIRST;
+	static final private int SETTINGS_ID = Menu.FIRST + 1;
 	public TextView latitudeField;
 	public TextView longitudeField;
 	public TextView cogField;
+	public TextView distanceField;
 	public Location last_location;
+
+	/**
+	 * void com.jakewendt.wherewasimarker.WhereWasIMarker.onInit(int initStatus)
+	 * 
+	 * @Override
+	 * 
+	 * Specified by: onInit(...) in OnInitListener
+	 * public abstract void onInit (int status)
+	 * Since: API Level 4
+	 * Called to signal the completion of the TextToSpeech engine initialization.
+	 * 
+	 * Parameters
+	 * status	SUCCESS or ERROR.
+	 */
+	@Override
+	public void onInit(int initStatus) {
+		Log.d(TAG,"onInit");
+		if (initStatus == TextToSpeech.SUCCESS) {
+			tts.speak( "Program in-ish-e-ated",TextToSpeech.QUEUE_FLUSH, null);
+		}
+	}
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG,"onCreate");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
 		latitudeField  = (TextView) findViewById(R.id.latitude);
 		longitudeField = (TextView) findViewById(R.id.longitude);
+		distanceField  = (TextView) findViewById(R.id.distance);
 		cogField       = (TextView) findViewById(R.id.cog);
+
+		// Initialize text-to-speech. This is an asynchronous operation.
+		// The OnInitListener (second argument) is called after initialization completes.
+		tts = new TextToSpeech( this, this );
 
 		// get a hangle on the location manager
 		locationManager =(LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		// public void requestLocationUpdates (String provider, long minTime, float minDistance, PendingIntent intent)
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000,
 				0, new LocationUpdateHandler());
-/*
- *         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000,
-        		1, new LocationUpdateHandler());
-*/
+
 		((Button) findViewById(R.id.decrease)).setOnClickListener(mDecreaseListener);
 		((Button) findViewById(R.id.increase)).setOnClickListener(mIncreaseListener);
 		((Button) findViewById(R.id.mark)).setOnClickListener(mMarkListener);
+//		((Button) findViewById(R.id.showlocation)).setOnClickListener(mShowListener);
 
-		// Restore preferences
-		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		speed = settings.getInt("speed", 15);
-		name  = settings.getString("name", "Jake's Speed Limit Marker");
-		email = settings.getString("email", "jake@example.com");
-
+		update_settings_view(this);
 		refresh_speed();
+
+/*
+		TextView sl = (TextView) findViewById(R.id.speedlimit);
+		Log.d(TAG,"Bottom " + Integer.toString(sl.getBottom()));
+		Log.d(TAG,"PaddingBottom " + Integer.toString(sl.getPaddingBottom()));
+		Log.d(TAG,"LineHeight " + Integer.toString(sl.getLineHeight()));
+		sl.setTextSize(TypedValue.COMPLEX_UNIT_SP,192);
+*/
+	}
+
+	public void update_settings_view(Context context) {
+		Log.d(TAG,"update_settings_view");
+		Settings.readSettings(context);
 	}
 
 	// this inner class is the intent reciever that recives notifcations
@@ -96,21 +135,21 @@ public class SpeedLimitMarker extends Activity {
 		public void onLocationChanged(Location location) {
 			refresh_location(location);
 		}
-
 //		@Override
 		public void onProviderDisabled(String provider) {}
-
 //		@Override
 		public void onProviderEnabled(String provider) {}
-
 //		@Override
 		public void onStatusChanged(String provider, int status,
 				Bundle extras) {}
 	}
 
-	public void showLocation(View view) {
-		switch (view.getId()) {
-		case R.id.showlocation:
+	/**
+	 * A call-back for when the user presses the show button.
+	 */
+	OnClickListener mShowListener = new OnClickListener() {
+		public void onClick(View v) {
+			Log.d(TAG,"mShowListener::onClick");
 			LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 			Location location = locationManager
 					.getLastKnownLocation(LocationManager.GPS_PROVIDER);
@@ -119,52 +158,74 @@ public class SpeedLimitMarker extends Activity {
 			} else {
 				latitudeField.setText("GPS not available");
 				longitudeField.setText("GPS not available");
+				cogField.setText("GPS not available");
+				distanceField.setText("GPS not available");
 			}
-			break;
 		}
-
-	}
+	};
 
 	@Override
 	protected void onStop(){
+		Log.d(TAG,"onStop");
+		tts.speak("Self destruct sequence in-ish-e-ated",TextToSpeech.QUEUE_FLUSH, null);
 		super.onStop();
-
-		// We need an Editor object to make preference changes.
-		// All objects are from android.context.Context
-		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		SharedPreferences.Editor editor = settings.edit();
-		editor.putInt("speed", speed);
-		editor.putString("name", name);
-		editor.putString("email", email);
-
-		// Commit the edits!
-		editor.commit();
 	}
 
-
 	public void refresh_location(Location location){
+		Log.d(TAG,"refresh_location");
 		last_location = location;
-/*    	
-		double lat = location.getLatitude();
-		double lng = location.getLongitude();
-		float  cog = location.getBearing();
-*/
 		latitudeField.setText(Double.toString( location.getLatitude()));
 		longitudeField.setText(Double.toString(location.getLongitude()));
-		cogField.setText(Float.toString(location.getBearing()));
+
+//		perhaps save a bearing location every minute to compare to
+/*
+ * float 	distanceTo(Location dest)
+ * Returns the approximate distance in meters between this location and the given location.
+ * 
+ * bull pucky. At walking speed, distanceTo and bearingTo are just wrong.  
+ * 
+ * float 	bearingTo(Location dest)
+ * Returns the approximate initial bearing in degrees East of true North when traveling 
+ * 	along the shortest path between this location and the given location.
+ */
+		
+		if( bearing_location == null ){
+			bearing_location = last_location;
+		}
+		// Returns the approximate distance in meters between this location and the given location.
+		distance = bearing_location.distanceTo(last_location);
+		distanceField.setText(Float.toString(distance));
+		if( distance > 0.1 ) {
+			last_bearing = bearing_location.bearingTo(last_location);
+			cogField.setText(Float.toString(last_bearing));
+			bearing_location = last_location;	
+		} else {
+//			cogField.setText("---");
+		}
+//		bearing_location = last_location;	
+//		tts.speak("Set location",TextToSpeech.QUEUE_FLUSH, null);
 	}
 
 	public void refresh_speed() {
-		((TextView) findViewById(R.id.speedlimit)).setText(Integer.toString(speed));
+		Log.d(TAG,"refresh_speed");
+		((TextView) findViewById(R.id.speedlimit)).setText(Integer.toString(Settings.speed_limit));
+		tts.speak( Integer.toString(Settings.speed_limit),
+				TextToSpeech.QUEUE_FLUSH, null);
 	}
 
 	public void increase_speed() {
-		speed += 5;
+		Log.d(TAG,"increase_speed");
+		if( Settings.speed_limit < 85 ){
+			Settings.speed_limit += 5;
+		}
 		refresh_speed();
 	}
 
 	public void decrease_speed() {
-		speed -= 5;
+		Log.d(TAG,"decrease_speed");
+		if( Settings.speed_limit > 5 ){
+			Settings.speed_limit -= 5;
+		}
 		refresh_speed();
 	}
 
@@ -173,6 +234,7 @@ public class SpeedLimitMarker extends Activity {
 	 */
 	OnClickListener mIncreaseListener = new OnClickListener() {
 		public void onClick(View v) {
+			Log.d(TAG,"mIncreaseListener::onClick");
 			increase_speed();
 		}
 	};
@@ -181,7 +243,8 @@ public class SpeedLimitMarker extends Activity {
 	 */
 	OnClickListener mDecreaseListener = new OnClickListener() {
 		public void onClick(View v) {
-				decrease_speed();
+			Log.d(TAG,"mDecreaseListener::onClick");
+			decrease_speed();
 		}
 	};
 	/**
@@ -189,67 +252,55 @@ public class SpeedLimitMarker extends Activity {
 	 */
 	OnClickListener mMarkListener = new OnClickListener() {
 		public void onClick(View v) {
-/*
-        		
-        	final int  myMph=  (int) (70);
-        	final double myLatitude= loc.getLatitude();
-        	final double myLongitude= loc.getLongitude();
-        	final float  myCog= loc.getBearing();
-        	
-            String name=       "Mr. Android";
-        	String direction=  Float.toString(myCog);
-        	String hours=      "";
-        	String email=      "";
-*/
-			try {
-				HttpClient httpclient = new DefaultHttpClient();
-				List<NameValuePair> formparams = new ArrayList<NameValuePair>();
-				formparams.add(new BasicNameValuePair("mlat", 
-					Double.toString(last_location.getLatitude())));
-				formparams.add(new BasicNameValuePair("mlon", 
-					Double.toString(last_location.getLongitude())));
-				formparams.add(new BasicNameValuePair("mmph", 
-					Integer.toString(speed)));
+			Log.d(TAG,"mMarkListener::onClick");
+			if (last_location == null) {
+				tts.speak( "No Location",TextToSpeech.QUEUE_FLUSH, null);
+			} else if ( Float.isNaN(last_bearing) ) {
+				tts.speak( "No Bearing",TextToSpeech.QUEUE_FLUSH, null);
+			} else {
+				tts.speak( "Marking Speed " + Integer.toString(Settings.speed_limit),
+					TextToSpeech.QUEUE_FLUSH, null);
+				try {
+					HttpClient httpclient = new DefaultHttpClient();
+					List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+					formparams.add(new BasicNameValuePair("mlat", 
+						Double.toString(last_location.getLatitude())));
+					formparams.add(new BasicNameValuePair("mlon", 
+						Double.toString(last_location.getLongitude())));
+					formparams.add(new BasicNameValuePair("mmph", 
+						Integer.toString(Settings.speed_limit)));
+	
+					formparams.add(new BasicNameValuePair("mtag", Settings.username));
+					formparams.add(new BasicNameValuePair("mcog", 
+						Double.toString(last_bearing)));
+					formparams.add(new BasicNameValuePair("mhours", ""));
+					formparams.add(new BasicNameValuePair("memail", Settings.email));
+	
+					UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, "UTF-8");
+//					HttpPost httppost = new HttpPost("http://www.wikispeedia.org/a/process_submit_bb.php");
+					HttpPost httppost = new HttpPost("http://wherewasi.jakewendt.com/markers");
+					httppost.setEntity(entity);
+					httpclient.execute(httppost);
 
-/*
-formparams.add(new BasicNameValuePair("mtag", name));
-formparams.add(new BasicNameValuePair("mcog", direction));
-formparams.add(new BasicNameValuePair("mhours", hours));
-formparams.add(new BasicNameValuePair("memail", email));
-*/
-				UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, "UTF-8");
-				HttpPost httppost = new HttpPost("http://www.wikispeedia.org/a/process_submit_bb.php");
-				httppost.setEntity(entity);
-//				httpclient.execute(httppost);
-			}catch (Exception e) {
-				e.printStackTrace();
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-
-			finish();
 		}
 	};
-
-	/**
-	 * Called when the activity is about to start interacting with the user.
-	 */
-	@Override
-	protected void onResume() {
-		super.onResume();
-	}
 
 	/**
 	 * Called when your activity's options menu needs to be created.
 	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		Log.d(TAG,"onCreateOptionsMenu");
 		super.onCreateOptionsMenu(menu);
-
 		// We are going to create two menus. Note that we assign them
 		// unique integer IDs, labels from our string resources, and
 		// given them shortcuts.
 		menu.add(0, BACK_ID, 0, R.string.back).setShortcut('0', 'b');
-		menu.add(0, CLEAR_ID, 0, R.string.clear).setShortcut('1', 'c');
-
+		menu.add(0, SETTINGS_ID, 0, R.string.settings).setShortcut('1', 's');
 		return true;
 	}
 
@@ -258,12 +309,11 @@ formparams.add(new BasicNameValuePair("memail", email));
 	 */
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
+		Log.d(TAG,"onPrepareOptionsMenu");
 		super.onPrepareOptionsMenu(menu);
-
 		// Before showing the menu, we need to decide whether the clear
 		// item is enabled depending on whether there is text to clear.
 		//        menu.findItem(CLEAR_ID).setVisible(mEditor.getText().length() > 0);
-
 		return true;
 	}
 
@@ -272,15 +322,16 @@ formparams.add(new BasicNameValuePair("memail", email));
 	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.d(TAG,"onOptionsItemSelected");
 		switch (item.getItemId()) {
 		case BACK_ID:
 			finish();
 			return true;
-		case CLEAR_ID:
-//			mEditor.setText("");
+		case SETTINGS_ID:
+			Intent ssettings = new Intent(this,Settings.class);
+			startActivity(ssettings);
 			return true;
 		}
-
 		return super.onOptionsItemSelected(item);
 	}
 
